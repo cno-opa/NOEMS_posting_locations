3061d0442eddaf7d87ae0dce980f195cd03cf313
Alex WIlliamson <abwilliamson@nola.gov>
2017-11-02 11:34:57 -0500

    moved countWithin to spatial.R

--- code/functions/estimate_coverage.R
+++ code/functions/estimate_coverage.R
@@ -60,10 +60,6 @@ poisson_test_p_value <- function(test_rate,comparison_rate) {
 	round( test$p.value, digits = 16)
 }
 
-
-# 
-# getNames <- function(data) data[, grepl("V",names( data))]
-# 
 calculate_multi_coverage <- function(p, c) {
   over(c, p, returnList = TRUE) %>%
     map_int(nrow) %>%
@@ -71,86 +67,34 @@ calculate_multi_coverage <- function(p, c) {
     summarise(triple_coverage = sum(value > 2),
               double_coverage = sum(value > 1))
 }
-# 
-# 
-# 
-# 
-# 
-# countWithin <- function(within, around, col.name = as.character(bquote(within)), 
-# 												return.type = "vec", ..., range) {
-# 	# Answers a common spatial analysis question: How many of one object are within
-# 	# another?  To adapt this function to answer similar questions, see help(over)
-# 	# within: points, the object to be counted around: polygons, boundaries in which
-# 	# to evaluate, or points to search near return.type: one of c('vec', 'poly',
-# 	# 'points'). If vec, returns a vector of the counts of within in around.
-# 	# Otherwise returns around with this vector appended to it, either as a
-# 	# SpatialPolygons or SpatialPoints object.  Can only use 'points' if return.type
-# 	# is a SpatialPoints object optional - range: if around is a SpatialPoints
-# 	# object, the area (in feet) to search around around
-# 	dots <- eval(substitute(alist(...)))
-# 	if (!is.null(dots$range)) {
-# 		range <- dots$range
-# 	}
-# 	
-# 	within.crs <- within@proj4string
-# 	around.crs <- around@proj4string
-# 	within <- spTransform(within, NOLA.proj)
-# 	around <- spTransform(around, NOLA.proj)
-# 	
-# 	if (is(around, "SpatialPoints") | is(around, "SpatialPointsDataFrame")) {
-# 		around.poly <- gBuffer(around, width = range, byid = TRUE)
-# 		count.within <- sapply(over(around.poly, geometry(within), returnList = TRUE), 
-# 													 length)
-# 	} else {
-# 		count.within <- sapply(over(around, geometry(within), returnList = TRUE), 
-# 													 length)
-# 		around.poly <- around
-# 	}
-# 	
-# 	# getName <- function(v1){deparse(substitute(v1))} getName <-
-# 	# function(v1){as.character(bquote(v1))} final.name <- paste0('count.of.',
-# 	# col.name)
-# 	
-# 	if (return.type == "points") {
-# 		around$poly.name <- count.within
-# 		names(around)[which(names(around) == "poly.name")] <- col.name
-# 		around <- spTransform(around, around.crs)
-# 		return(around)
-# 	} else if (return.type == "poly") {
-# 		around.poly$poly.name <- count.within
-# 		names(around.poly)[which(names(around.poly) == "poly.name")] <- col.name
-# 		around.poly <- spTransform(around.poly, around.crs)
-# 		return(around.poly)
-# 	} else if (return.type == "vec") {
-# 		return(count.within)
-# 	}
-# }
-# 
-# order.by.coverage <- function(spatial.points, polygons) {
-# 	#The following function chooses polygon of greatest impact, given call points and a set of candidate polygons
-# 	ordered.polygons <- polygons
-# 	ordered.polygons$CallCount <- countWithin(spatial.points, ordered.polygons)
-# 	
-# 	ordered.polygons <- ordered.polygons[order(ordered.polygons$CallCount, decreasing = TRUE),]
-# 	
-# 	return(ordered.polygons)
-# }
+
+
+
+order_by_coverage <- function(spatial_points, polygons) {
+	#The following function chooses polygon of greatest impact, given call points and a set of candidate polygons
+	ordered_polygons <- polygons
+	ordered_polygons$CallCount <- countWithin(spatial_points, ordered_polygons)
+
+	ordered_polygons <- ordered_polygons[order(ordered_polygons$CallCount, decreasing = TRUE),]
+
+	return(ordered_polygons)
+}
 # 
 # rank.polygon.impact <- function(impact.points, polygons, rank.to = 2, seed.polygons = NULL) {
 # 	#Sequentially finds set of N polygons with most coverage.
-# 	
+# 
 # 	initial.polygons <- if (is.null(seed.polygons)) polygons else seed.polygons
-# 	
+# 
 # 	ordered.set <- order.by.coverage(impact.points, initial.polygons)
-# 	
+# 
 # 	solution.set <-  if (is.null(seed.polygons)) {
 # 		ordered.set[1,]
-# 	} else 
-# 		rank.polygon.impact(impact.points, 
-# 												seed.polygons, 
-# 												rank.to = nrow(seed.polygons@data) 
+# 	} else
+# 		rank.polygon.impact(impact.points,
+# 												seed.polygons,
+# 												rank.to = nrow(seed.polygons@data)
 # 		)
-# 	
+# 
 # 	remaining.polygons <- remove.by.NewName(solution.set$NewName, polygons)
 # 	if (rank.to > 1){
 # 		for (i in 2:rank.to) {
@@ -161,7 +105,7 @@ calculate_multi_coverage <- function(p, c) {
 # 			remaining.polygons <- remove.by.NewName(next.greatest.coverage.polygon$NewName, remaining.polygons)
 # 		}
 # 	}
-# 	
+# # 	
 # 	
 # 	
 # 	return(solution.set)
